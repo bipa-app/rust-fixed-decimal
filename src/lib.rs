860bc36454896b823ab0d5dd77274e53378491f4
Guilherme Stabach Salustiano <guissalustiano@gmail.com>
2024-10-05 02:59:55 -0300

    impl new and Display

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,105 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+mod constants;
+mod str;
+
+use core::fmt;
+
+// TODO: make it private
+pub trait Num: Copy + num_traits::Euclid + num_traits::Zero + From<u8> {
+    type Unsigned: Num + num_traits::Unsigned;
+
+    fn abs(self) -> u128;
+    fn is_positive(self) -> bool;
+    fn is_negative(self) -> bool {
+        !self.is_positive()
+    }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl Num for i128 {
+    type Unsigned = u128;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn abs(self) -> Self::Unsigned {
+        self.abs() as u128
+    }
+
+    fn is_positive(self) -> bool {
+        self >= 0
+    }
+}
+impl Num for u128 {
+    type Unsigned = Self;
+
+    fn abs(self) -> Self {
+        self
+    }
+    fn is_positive(self) -> bool {
+        true
+    }
+}
+
+/// `FixedDecimal` represents a 128 or 64 bits representation of a fixed-precision decimal number.
+/// The finite set of values of type `FixedDecimal` are of the form m / 10<sup>E</sup>,
+/// where m is an integer `T`, and e is an i32
+pub struct FixedDecimal<T: Num, const SCALE: u8>(T);
+
+/// `Decimal` represents a 128 bit representation of a fixed-precision decimal number.
+/// The finite set of values of type `Decimal` are of the form m / 10<sup>e</sup>,
+/// where m is an integer such that -2<sup>96</sup> < m < 2<sup>96</sup>, and e is i32.
+pub type FixedDecimalI128<const E: i32> = FixedDecimal<i128, E>;
+
+pub type FixedDecimalI128P2 = FixedDecimal<i128, 2>;
+pub type FixedDecimalI128P4 = FixedDecimal<i128, 4>;
+
+impl<T: Num, const SCALE: u8> FixedDecimal<T, SCALE> {
+    /// Returns a `FixedDecimal` with `m` representation and corresponding `E` scale.
+    ///
+    /// # Arguments
+    ///
+    /// * `num` - An T that represents the `m` portion of the decimal number
+    /// * `E` - A i32 representing the `e` portion of the decimal number.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// # use rust_fixed_decimal::FixedDecimalI128P4;
+    /// #
+    /// let pi = FixedDecimalI128P4::new(31415);
+    /// assert_eq!(pi.to_string(), "3.1415");
+    /// ```
+    #[must_use]
+    pub fn new(num: T) -> Self {
+        Self(num)
+    }
+
+    #[must_use]
+    pub(crate) const fn mantissa(&self) -> T {
+        self.0
+    }
+
+    #[must_use]
+    pub(crate) fn is_positive(&self) -> bool {
+        self.0.is_positive()
+    }
+
+    #[must_use]
+    pub(crate) fn is_negative(&self) -> bool {
+        self.0.is_negative()
+    }
+}
+
+impl<T: Num, const E: u8> fmt::Display for FixedDecimal<T, E> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        let (rep, additional) = crate::str::to_str_internal(self, false, f.precision());
+        if let Some(additional) = additional {
+            let value = [rep.as_str(), "0".repeat(additional).as_str()].concat();
+            f.pad_integral(self.is_positive(), "", value.as_str())
+        } else {
+            f.pad_integral(self.is_positive(), "", rep.as_str())
+        }
+    }
+}
+
+impl<T: Num, const E: u8> fmt::Debug for FixedDecimal<T, E> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        fmt::Display::fmt(self, f)
     }
 }
---
+++ src/str.rs
@@ -0,0 +1,141 @@
+use crate::{constants::MAX_STR_BUFFER_SIZE, FixedDecimal, Num};
+
+use arrayvec::ArrayString;
+
+use std::{char, cmp::min};
+
+// impl that doesn't allocate for serialization purposes.
+pub(crate) fn to_str_internal<T: Num, const SCALE: u8>(
+    value: &FixedDecimal<T, SCALE>,
+    append_sign: bool,
+    precision: Option<usize>,
+) -> (ArrayString<MAX_STR_BUFFER_SIZE>, Option<usize>) {
+    let total_len = value.mantissa().abs().ilog10() + 1; // 4
+    let scale: usize = SCALE.into();
+    let zero_len = scale.saturating_sub(total_len as usize);
+    let (prec, prec_rem) = match precision {
+        Some(prec) => {
+            let max: usize = u8::MAX.into();
+            if prec > max {
+                (max, Some(prec - max))
+            } else {
+                (prec, None)
+            }
+        }
+        None => (SCALE.into(), None),
+    };
+
+    let mut rep = ArrayString::<MAX_STR_BUFFER_SIZE>::new();
+
+    if append_sign && value.is_negative() {
+        rep.push('-');
+    }
+
+    let add_digit = |rep: &mut ArrayString<MAX_STR_BUFFER_SIZE>, i| {
+        let digit = ((value.mantissa().abs() / 10u128.pow(i)) % 10) as u8;
+        rep.push(char::from(b'0' + digit));
+    };
+
+    if total_len <= SCALE.into() {
+        rep.push('0');
+        rep.push('.');
+        for _ in 0..min(zero_len, prec) {
+            rep.push('0');
+        }
+        for i in (0..total_len).rev().take(prec.saturating_sub(zero_len)) {
+            add_digit(&mut rep, i);
+        }
+    } else {
+        for i in (SCALE.into()..total_len).rev() {
+            add_digit(&mut rep, i);
+        }
+        if prec != 0 {
+            rep.push('.');
+            for i in (0u32..SCALE.into()).rev() {
+                add_digit(&mut rep, i);
+            }
+            for _ in 0..(prec.saturating_sub(SCALE.into())) {
+                rep.push('0');
+            }
+        }
+    }
+
+    (rep, prec_rem)
+}
+
+#[test]
+fn aaaa() {
+    assert_eq!(
+        to_str_internal(&FixedDecimal::<i128, 0>(1234), true, None)
+            .0
+            .as_str(),
+        "1234"
+    );
+}
+#[test]
+fn bbbbb() {
+    assert_eq!(
+        to_str_internal(&FixedDecimal::<i128, 0>(-1234), true, None)
+            .0
+            .as_str(),
+        "-1234"
+    );
+}
+#[test]
+fn cccccccc() {
+    assert_eq!(
+        to_str_internal(&FixedDecimal::<i128, 1>(1234), true, None)
+            .0
+            .as_str(),
+        "123.4"
+    );
+}
+#[test]
+fn d() {
+    assert_eq!(
+        to_str_internal(&FixedDecimal::<i128, 5>(1234), true, None)
+            .0
+            .as_str(),
+        "0.01234"
+    );
+}
+
+#[test]
+fn e() {
+    assert_eq!(
+        to_str_internal(&FixedDecimal::<i128, 4>(1234), true, None)
+            .0
+            .as_str(),
+        "0.1234"
+    );
+}
+
+#[test]
+fn f() {
+    assert_eq!(
+        to_str_internal(&FixedDecimal::<i128, 5>(1234), true, Some(2))
+            .0
+            .as_str(),
+        "0.01"
+    );
+}
+
+#[test]
+fn g() {
+    assert_eq!(
+        to_str_internal(&FixedDecimal::<i128, 0>(1234), true, Some(2))
+            .0
+            .as_str(),
+        "1234.00"
+    );
+}
+
+#[test]
+fn h() {
+    assert_eq!(
+        to_str_internal(&FixedDecimal::<i128, 5>(1), true, Some(2))
+            .0
+            .as_str(),
+        "0.00"
+    );
+}
